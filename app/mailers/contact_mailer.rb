e9e2ec80bd859297bafb7c0442e33a4172b52e09
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-03-28 11:41:39 +0000

    ユーザー名変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to: "kaori_toyonaga@yahoo.co.jp", subject: "お問い合わせの確認メール"
+    mail to: 'kaori_toyonaga@yahoo.co.jp', subject: "お問い合わせの確認メール"
   end
 end
